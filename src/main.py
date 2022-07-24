29696984d0b45fcec3fb0c28c23a9f75f7d44119
GuilhermeBraz <gvcerveirabraz@gmail.com>
2022-07-24 17:38:12 -0300

    Update main and userInterface
    Co-authored-by: RafaelBP02 <rafaelbertopereira@gmail.com>

--- src/main.py
+++ src/main.py
@@ -1,6 +1,18 @@
 from userInterface import TelaAgenda
-import datetime
+from event import Event
+from greedyAlgorithm import interval_scheduling
 
 
 if __name__ == "__main__":
     events = TelaAgenda.start()
+    # print all events and their start and end time
+    for event in events:
+        print(event)
+
+    # schedule events
+    schedule = interval_scheduling(events)
+    print("\n\n\n")
+    print("Schedule:")
+    for event in schedule:
+        print(event)
+    print("\n\n\n")
